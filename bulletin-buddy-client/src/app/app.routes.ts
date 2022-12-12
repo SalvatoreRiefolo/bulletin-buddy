1af169915688eb9f8c4b8555ab6c4bec85d1e788
skrhakv <v.skrhak@gmail.com>
2022-12-12 14:15:50 +0100

    wow I should run the solution before doing commit

--- bulletin-buddy-client/src/app/app.routes.ts
+++ bulletin-buddy-client/src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from "@angular/router";
 import { LandingPageComponent } from "./landing-page/landing-page.component";
 import { LoginPageComponent } from "./login-page/login-page.component";
 import { OverviewPageComponent } from "./overview-page/overview-page.component";
-import { OverviewPageComponent } from "./overview-page/overview-page.component";
 
 export const ROUTES: Routes = [
     { path: '', component: LandingPageComponent },
