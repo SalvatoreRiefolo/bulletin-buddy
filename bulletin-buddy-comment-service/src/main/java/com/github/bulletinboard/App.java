d7daeb7cdcf7bd23eed2517562c83e716f00cafd
clarari <119805330+clarari@users.noreply.github.com>
2022-12-19 18:36:03 +0100

    connect CommentService to CommentRepository

--- bulletin-buddy-comment-service/src/main/java/com/github/bulletinboard/App.java
+++ bulletin-buddy-comment-service/src/main/java/com/github/bulletinboard/App.java
@@ -1,13 +1,8 @@
 package com.github.bulletinboard;
 
-import com.github.bulletinboard.models.Comment;
-import com.github.bulletinboard.repositories.CommentRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
-import java.util.UUID;
+
 
 @SpringBootApplication
 public class App {
--- bulletin-buddy-comment-service/src/main/java/com/github/bulletinboard/services/CommentService.java
+++ bulletin-buddy-comment-service/src/main/java/com/github/bulletinboard/services/CommentService.java
@@ -1,30 +1,25 @@
 package com.github.bulletinboard.services;
 
 import com.github.bulletinboard.models.Comment;
+import com.github.bulletinboard.repositories.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
 
 @Service
 public class CommentService {
-    private final HashMap<UUID, ArrayList<Comment>> comments;
 
-    public CommentService() {
-        this.comments = new HashMap<>();
-    }
+    @Autowired
+    private CommentRepository commentRepository;
 
     public Comment[] getCommentsByOfferId(UUID offerId){
-        List<Comment> matches = comments.getOrDefault(offerId, new ArrayList<>());
+        List<Comment> matches = commentRepository.findByOfferId(offerId);
         return matches.toArray(Comment[]::new);
     }
 
     public void addComment(Comment comment){
-        UUID offerId = comment.getOfferId();
-        ArrayList<Comment> matches = comments.getOrDefault(offerId, new ArrayList<>());
-        matches.add(comment);
-        comments.put(offerId, matches);
+        commentRepository.saveAndFlush(comment);
     }
 }
