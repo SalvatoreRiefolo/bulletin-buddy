364df0e7b620d2d121cb198669c436ca29b1dfce
SalvatoreRiefolo <38936476+SalvatoreRiefolo@users.noreply.github.com>
2022-12-24 23:29:26 +0100

    updated global paths

--- bulletin-buddy-client/src/app/global.ts
+++ bulletin-buddy-client/src/app/global.ts
@@ -1,3 +1,3 @@
-export const commentServiceUrl = 'https://bulletin-buddy-comment-service-production.up.railway.app';
-export const authenticationServiceUrl = 'https://bulletin-buddy-authentication-service-production.up.railway.app';
-export const offerServiceUrl = 'https://bulletin-buddy-offer-service-production.up.railway.app';
+export const commentServiceUrl = 'https://bulletin-buddy-comment-service-production.up.railway.app/';
+export const authenticationServiceUrl = 'https://bulletin-buddy-authentication-service-production.up.railway.app/';
+export const offerServiceUrl = 'https://bulletin-buddy-offer-service-production.up.railway.app/';
