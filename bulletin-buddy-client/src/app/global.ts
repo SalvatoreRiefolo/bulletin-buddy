5b904a2c0374e8dbdaea3be2d2586779d8b1d892
SalvatoreRiefolo <38936476+SalvatoreRiefolo@users.noreply.github.com>
2022-12-24 23:07:36 +0100

    fixed type

--- bulletin-buddy-client/src/app/global.ts
+++ bulletin-buddy-client/src/app/global.ts
@@ -1,3 +1,3 @@
 export var commentServiceUrl:string = 'http://localhost:8083/';
 export var authenticationServiceUrl:string = 'http://localhost:8082/';
-export var offerServiceUrl:string = process.env["OFFER_URL"]
+export var offerServiceUrl:string = process.env["OFFER_URL"]!
