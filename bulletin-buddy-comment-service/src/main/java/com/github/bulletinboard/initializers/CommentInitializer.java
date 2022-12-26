2c6a14739eb0c7df69ec98d762431d2a07b4ef5e
clarari <cmricken2@gmail.com>
2022-12-26 16:30:19 +0100

    Improve example data

--- bulletin-buddy-comment-service/src/main/java/com/github/bulletinboard/initializers/CommentInitializer.java
+++ bulletin-buddy-comment-service/src/main/java/com/github/bulletinboard/initializers/CommentInitializer.java
@@ -11,35 +11,38 @@ import java.util.UUID;
 @Component
 public class CommentInitializer implements InitializingBean {
 
+    private final UUID OFFER_ID_1 = UUID.fromString("bb27a1b0-9602-4d1c-9a76-6ab5dca69837");
+    private final UUID OFFER_ID_2 = UUID.fromString("1240ceb0-2d1b-4fb3-89e1-c30089b5222f");
+
     @Autowired
     private CommentService commentService;
 
     @Override
     public void afterPropertiesSet() throws Exception {
         Comment c1 = new Comment();
-        c1.setPostId(UUID.fromString("bb27a1b0-9602-4d1c-9a76-6ab5dca69837"));
-        c1.setPosterEmail("test@test.com");
-        c1.setContent("Comment 1");
+        c1.setPostId(OFFER_ID_1);
+        c1.setPosterEmail("claudia.tester@test.com");
+        c1.setContent("Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.");
 
         Comment c2 = new Comment();
-        c2.setPostId(UUID.fromString("bb27a1b0-9602-4d1c-9a76-6ab5dca69837"));
-        c2.setPosterEmail("test@test.com");
-        c2.setContent("Comment 2");
+        c2.setPostId(OFFER_ID_1);
+        c2.setPosterEmail("gerhard.testposter@test.com");
+        c2.setContent("Lorem ipsum dolor sit amet, consetetur sadipscing elitr.");
 
         Comment c3 = new Comment();
-        c3.setPostId(UUID.fromString("bb27a1b0-9602-4d1c-9a76-6ab5dca69837"));
-        c3.setPosterEmail("test@test.com");
-        c3.setContent("Comment 3");
+        c3.setPostId(OFFER_ID_1);
+        c3.setPosterEmail("jane.doe@test.com");
+        c3.setContent("No sea takimata sanctus est Lorem ipsum dolor sit amet.");
 
         Comment c4 = new Comment();
-        c4.setPostId(UUID.fromString("1240ceb0-2d1b-4fb3-89e1-c30089b5222f"));
-        c4.setPosterEmail("test@test.com");
-        c4.setContent("Comment 4");
+        c4.setPostId(OFFER_ID_2);
+        c4.setPosterEmail("claudia.tester@test.com");
+        c4.setContent(" At vero eos et accusam et justo duo dolores et ea rebum.");
 
         Comment c5 = new Comment();
-        c5.setPostId(UUID.fromString("1240ceb0-2d1b-4fb3-89e1-c30089b5222f"));
-        c5.setPosterEmail("test@test.com");
-        c5.setContent("Comment 5");
+        c5.setPostId(OFFER_ID_2);
+        c5.setPosterEmail("gerhard.testposter@test.com");
+        c5.setContent("Dolor sit amet.");
 
         commentService.addComment(c1);
         commentService.addComment(c2);
--- bulletin-buddy-post-service/src/main/java/com/github/bulletinboard/initializers/PostInitializer.java
+++ bulletin-buddy-post-service/src/main/java/com/github/bulletinboard/initializers/PostInitializer.java
@@ -18,27 +18,33 @@ public class PostInitializer implements InitializingBean {
     public void afterPropertiesSet() throws Exception {
         Post p1 = new Post();
         p1.setId(UUID.fromString("bb27a1b0-9602-4d1c-9a76-6ab5dca69837"));
-        p1.setTitle("Post 1 Title");
-        p1.setBody("Post 1 Body");
-        p1.setPublisherEmail("test@test.com");
+        p1.setTitle("Takimata");
+        p1.setBody("Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.");
+        p1.setPublisherEmail("franz.poster@test.com");
+        p1.setType(Post.PostType.REQUEST);
 
         Post p2 = new Post();
         p2.setId(UUID.fromString("1240ceb0-2d1b-4fb3-89e1-c30089b5222f"));
-        p2.setTitle("Post 2 Title");
-        p2.setBody("Post 2 Body");
-        p2.setPublisherEmail("test@test.com");
+        p2.setTitle("Ut labore");
+        p2.setBody("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, " +
+                "sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.");
+        p2.setPublisherEmail("jane.doe@test.com");
+        p2.setType(Post.PostType.OFFER);
 
         Post p3 = new Post();
         p3.setId(UUID.fromString("fb32d795-1cee-4935-82a9-74b45a0d826e"));
-        p3.setTitle("Post 3 Title");
-        p3.setBody("Post 3 Body");
-        p3.setPublisherEmail("test@test.com");
+        p3.setTitle("kasd gubergren");
+        p3.setBody("At vero eos et accusam et justo duo dolores et ea rebum." +
+                " Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.");
+        p3.setPublisherEmail("gerhard.testposter@test.com");
+        p3.setType(Post.PostType.REQUEST);
 
         Post p4 = new Post();
         p4.setId(UUID.fromString("2019c418-711d-488f-8853-822417551f69"));
-        p4.setTitle("Post 4 Title");
-        p4.setBody("Post 4 Body");
-        p4.setPublisherEmail("test@test.com");
+        p4.setTitle("Vero accusam");
+        p4.setBody("At vero eos et accusam et justo duo dolores et ea rebum. ");
+        p4.setPublisherEmail("claudia.tester@test.com");
+        p4.setType(Post.PostType.OFFER);
 
         postService.addPost(p1);
         postService.addPost(p2);
