545d0b27fdea6bfa378a8982c08a521ad70d0591
skrhakv <v.skrhak@gmail.com>
2022-12-30 13:36:44 +0100

    Implement registration functionality

--- bulletin-buddy-client/src/app/app.routes.ts
+++ bulletin-buddy-client/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { LandingPageComponent } from "./landing-page/landing-page.component";
 import { LoginPageComponent } from "./login-page/login-page.component";
 import { LogoutPageComponent } from "./logout-page/logout-page.component";
 import { OverviewPageComponent } from "./overview-page/overview-page.component";
+import { RegisterPageComponent } from "./register-page/register-page.component";
 
 export const ROUTES: Routes = [
     { path: '', component: LandingPageComponent },
     { path: 'login', component: LoginPageComponent },
     { path: 'overview', component: OverviewPageComponent },
     { path: 'post/:id', component: DetailPageComponent },
-    { path: 'logout', component: LogoutPageComponent},
+    { path: 'logout', component: LogoutPageComponent },
+    { path: 'register', component: RegisterPageComponent },
 ];
