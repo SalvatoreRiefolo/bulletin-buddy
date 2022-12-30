4b66ab6a1870c42ad95226023cbbf1bf343157d3
skrhakv <v.skrhak@gmail.com>
2022-12-30 13:43:44 +0100

    Fix tests, add Register button to the landing page

--- bulletin-buddy-client/src/app/register-page/register-page.component.spec.ts
+++ bulletin-buddy-client/src/app/register-page/register-page.component.spec.ts
@@ -1,4 +1,6 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { RegisterPageComponent } from './register-page.component';
 
@@ -8,7 +10,8 @@ describe('RegisterPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RegisterPageComponent ]
+      declarations: [ RegisterPageComponent ],
+      imports: [ReactiveFormsModule, HttpClientTestingModule]
     })
     .compileComponents();
 
