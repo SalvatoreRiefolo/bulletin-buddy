35b61a27e1ef79868d2d9859354c8cdc6f79ee7c
clarari <cmricken2@gmail.com>
2023-01-08 15:28:43 +0100

    fix initializing data

--- sql/fill_tables.sql
+++ sql/fill_tables.sql
@@ -5,36 +5,36 @@ VALUES
 (
     '{d4215f86-cd95-41f5-b539-d2d408bb8055}',
     '{bb27a1b0-9602-4d1c-9a76-6ab5dca69837}',
-    'claudia.tester@test.com',
     'Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+    'claudia.tester@test.com',
     current_timestamp
 ),
 (
     '{682c92c6-2a8c-4b48-9359-4d92826845c2}',
     '{bb27a1b0-9602-4d1c-9a76-6ab5dca69837}',
-    'gerhard.testposter@test.com',
     'Lorem ipsum dolor sit amet, consetetur sadipscing elitr.',
+    'gerhard.testposter@test.com',
     current_timestamp
 ),
 (
     '{0ea3fd1f-35b9-4d12-94cc-7d429e9b60d5}',
     '{bb27a1b0-9602-4d1c-9a76-6ab5dca69837}',
-    'jane.doe@test.com',
     'No sea takimata sanctus est Lorem ipsum dolor sit amet.',
+    'jane.doe@test.com',
     current_timestamp
 ),
 (
     '{6ca3389f-babd-4e42-b8dc-3424fb2bd5ce}',
     '{1240ceb0-2d1b-4fb3-89e1-c30089b5222f}',
-    'claudia.tester@test.com',
     'At vero eos et accusam et justo duo dolores et ea rebum.',
+    'claudia.tester@test.com',
     current_timestamp
 ),
  (
     '{e64d43d9-054c-43ae-89a4-8b5695207cd0}',
     '{1240ceb0-2d1b-4fb3-89e1-c30089b5222f}',
-    'gerhard.testposter@test.com',
     'Dolor sit amet.',
+    'gerhard.testposter@test.com',
     current_timestamp
  )
 ;
