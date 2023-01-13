94cd37c44ed7d2c7bbb0f7a2ff9bf6652bac14bf
Katharina Stengg <katharina.stengg@student.tugraz.at>
2023-01-13 10:44:26 +0100

    add select to differ between sorting option of comments

--- bulletin-buddy-client-service/bulletin-buddy-client-service-web/src/app/detail-page/detail-page.component.ts
+++ bulletin-buddy-client-service/bulletin-buddy-client-service-web/src/app/detail-page/detail-page.component.ts
@@ -17,6 +17,8 @@ export class DetailPageComponent implements OnInit {
   public commentInput: string;
   private id: string;
 
+  public sort_option: string;
+
   constructor(
     private route: ActivatedRoute,
     private postsService: PostsService,
@@ -25,6 +27,7 @@ export class DetailPageComponent implements OnInit {
     this.post = new Post();
     this.comments = [];
     this.id = this.route.snapshot.params['id'];
+    this.sort_option = "NO";
     this.postsService.getPost(this.id).subscribe((data: Post) => {
       this.post = data;
     });
@@ -44,7 +47,11 @@ export class DetailPageComponent implements OnInit {
 
     this.commentsService.addComment(comment).subscribe(c => this.comments.push(comment));
   }
+
+  sort_func(event: string){
+    this.sort_option = event;
+    console.log(this.sort_option);
+  }
   ngOnInit(): void {
   }
-
 }
