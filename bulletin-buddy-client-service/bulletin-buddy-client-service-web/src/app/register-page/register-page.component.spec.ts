b3b85475a404fd5cbc759618416456aca5ab554c
skrhakv <v.skrhak@gmail.com>
2023-01-15 20:26:42 +0100

    Add unit tests in other components

--- bulletin-buddy-client-service/bulletin-buddy-client-service-web/src/app/register-page/register-page.component.spec.ts
+++ bulletin-buddy-client-service/bulletin-buddy-client-service-web/src/app/register-page/register-page.component.spec.ts
@@ -1,18 +1,31 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
+import { of } from 'rxjs/internal/observable/of';
+import { AuthenticationService } from '../authentication.service';
 
 import { RegisterPageComponent } from './register-page.component';
 
 describe('RegisterPageComponent', () => {
   let component: RegisterPageComponent;
   let fixture: ComponentFixture<RegisterPageComponent>;
-
+  let validEmail: string = "valid@email.com";
+  class MockAuthenticationService {
+    validateEmail(email: string) {
+      if(email === validEmail)
+        return of(true);
+      else
+        return of(false);
+    }
+  }
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ RegisterPageComponent ],
-      imports: [ReactiveFormsModule, HttpClientTestingModule]
-    })
+      imports: [ReactiveFormsModule, HttpClientTestingModule],
+      providers:[
+      { provide: AuthenticationService, useClass: MockAuthenticationService },
+
+    ]})
     .compileComponents();
 
     fixture = TestBed.createComponent(RegisterPageComponent);
@@ -22,5 +35,14 @@ describe('RegisterPageComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should validate email', () => {
+    component.passwordForm.value.email = validEmail;
+    component.emailChanged();
+    expect(component.validEmail).toBeTruthy();
+  });
+  it('should not validate email', () => {
+    component.emailChanged();
+    expect(component.validEmail).toBeFalsy();
   });
 });
