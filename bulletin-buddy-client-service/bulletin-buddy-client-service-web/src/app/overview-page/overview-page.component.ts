c58bbe13a6aa2573650d8b670f900be08095ea82
Katharina Stengg <katharina.stengg@student.tugraz.at>
2023-01-15 20:21:42 +0100

    changed filtering to a better approach

--- bulletin-buddy-client-service/bulletin-buddy-client-service-web/src/app/overview-page/overview-page.component.ts
+++ bulletin-buddy-client-service/bulletin-buddy-client-service-web/src/app/overview-page/overview-page.component.ts
@@ -10,6 +10,7 @@ import { PostsService } from '../posts.service';
 })
 export class OverviewPageComponent implements OnInit {
   public posts: Post[] = [];
+  public filtered_posts: Post[] = [];
 
   public postTypes = Object.values(PostType).filter(value => typeof value !== 'number');
 
@@ -18,12 +19,21 @@ export class OverviewPageComponent implements OnInit {
   constructor(private postsService: PostsService, public authenticationService: AuthenticationService) {
     this.postsService.getOverviewPosts().subscribe((data: Post[]) => {
       this.posts = data;
+      this.filtered_posts = data;
     });
   }
 
   filter_change(event: string){
     this.filter_option = event;
     console.log(this.filter_option);
+    if (this.filter_option == 'ALL')
+    {
+      this.filtered_posts = this.posts;
+    }
+    else
+    {
+      this.filtered_posts = this.posts.filter(value => value.type.toString() == this.filter_option);
+    }
   }
 
   ngOnInit(): void {
