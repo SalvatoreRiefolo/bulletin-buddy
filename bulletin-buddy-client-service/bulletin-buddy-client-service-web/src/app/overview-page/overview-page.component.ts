71f38bd5469f3ddff57e0b2ba7c97bf04b873200
Katharina Stengg <katharina.stengg@student.tugraz.at>
2023-01-18 16:35:46 +0100

    function to sort posts regarding the amount of their comments

--- bulletin-buddy-client-service/bulletin-buddy-client-service-web/src/app/overview-page/overview-page.component.ts
+++ bulletin-buddy-client-service/bulletin-buddy-client-service-web/src/app/overview-page/overview-page.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../authentication.service';
 import {Post, PostType} from '../post.model';
 import { PostsService } from '../posts.service';
+import {PostComment} from "../post-comment.model";
+import {CommentsService} from "../comments.service";
+import {map, Observable} from "rxjs";
 
 @Component({
   selector: 'app-overview-page',
@@ -16,7 +19,8 @@ export class OverviewPageComponent implements OnInit {
 
   public filterOption: string = "ALL";
 
-  constructor(private postsService: PostsService, public authenticationService: AuthenticationService) {
+  constructor(private postsService: PostsService, public authenticationService: AuthenticationService,
+              private commentsService: CommentsService) {
     this.postsService.getOverviewPosts().subscribe((data: Post[]) => {
       this.posts = data;
       this.filteredPosts = data;
@@ -34,6 +38,19 @@ export class OverviewPageComponent implements OnInit {
     {
       this.filteredPosts = this.posts.filter(value => value.type.toString() == this.filterOption);
     }
+  }
+
+  sort(event: string){
+    // TODO method for: sort posts according to comment count (the one with the most comments first)
+    // TODO improve maybe the getCount function
+    function getCount(comments: Observable<PostComment[]>): number {
+      let counter: number = 0;
+      comments.pipe(map((result: string | any[]) => {counter = result.length}))
+      return counter;
+    }
+    this.filteredPosts = this.filteredPosts.sort((one, two) =>
+      (getCount(this.commentsService.getComments(one.id)) > getCount(this.commentsService.getComments(two.id))
+        ? 1 : -1));
   }
 
   ngOnInit(): void {
