c0dd74fd41dde744558f583a66163fc457c14909
Katharina Stengg <katharina.stengg@student.tugraz.at>
2023-01-20 01:21:22 +0100

    WIP sort posts according to most comments

--- bulletin-buddy-client-service/bulletin-buddy-client-service-web/src/app/overview-page/overview-page.component.ts
+++ bulletin-buddy-client-service/bulletin-buddy-client-service-web/src/app/overview-page/overview-page.component.ts
@@ -18,6 +18,7 @@ export class OverviewPageComponent implements OnInit {
   public postTypes = Object.values(PostType).filter(value => typeof value !== 'number');
 
   public filterOption: string = "ALL";
+  public sortOption: string = "DEFAULT";
 
   constructor(private postsService: PostsService, public authenticationService: AuthenticationService,
               private commentsService: CommentsService) {
@@ -41,16 +42,55 @@ export class OverviewPageComponent implements OnInit {
   }
 
   sort(event: string){
+    this.sortOption = event;
     // TODO method for: sort posts according to comment count (the one with the most comments first)
     // TODO improve maybe the getCount function
     function getCount(comments: Observable<PostComment[]>): number {
-      let counter: number = 0;
-      comments.pipe(map((result: string | any[]) => {counter = result.length}))
-      return counter;
+      console.log("in get count");
+      let count: number = 0;
+      /*comments.pipe(map((result: string | any[]) => {let count = result.length;
+        console.log(result.length+"test")}))*/
+      comments.subscribe((data: PostComment[]) => {
+        count = data.length;
+        console.log(data.length);
+        console.log(count);
+        return data.length;
+      });
+
+        //x => {count = x.length; console.log(x.length)});
+      //console.log("HELLO");
+      //console.log(count);
+      return count;
     }
-    this.filteredPosts = this.filteredPosts.sort((one, two) =>
-      (getCount(this.commentsService.getComments(one.id)) > getCount(this.commentsService.getComments(two.id))
-        ? 1 : -1));
+
+    const getCommentsArray = (id: string): PostComment[] => {
+      let comments: PostComment[] = [];
+      this.commentsService.getComments(id).subscribe((data: PostComment[]) => {
+        comments = data;
+      });
+      console.log(id);
+      console.log(comments.length);
+      return comments;
+    }
+
+    console.log(this.sortOption);
+
+    if (this.sortOption == "DESC"){
+      console.log("in desc");
+
+      /*this.filteredPosts = this.filteredPosts.sort((one, two) =>
+        (getCount(this.commentsService.getComments(one.id)) > getCount(this.commentsService.getComments(two.id))
+          ? -1 : 1));*/
+      this.filteredPosts = this.filteredPosts.sort((one, two) =>
+        (getCommentsArray(one.id).length > getCommentsArray(two.id).length)
+          ? -1 : 1);
+    }
+    else
+    {
+      console.log("Default value");
+      this.filter(this.filterOption);
+    }
+
   }
 
   ngOnInit(): void {
