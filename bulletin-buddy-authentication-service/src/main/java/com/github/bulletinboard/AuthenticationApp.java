a717621dac4af975246a8b7b5d44c0fc59f0dd44
clarari <cmricken2@gmail.com>
2023-01-21 18:16:07 +0100

    make authentication service register with consul

--- bulletin-buddy-authentication-service/src/main/java/com/github/bulletinboard/AuthenticationApp.java
+++ bulletin-buddy-authentication-service/src/main/java/com/github/bulletinboard/AuthenticationApp.java
@@ -2,8 +2,10 @@ package com.github.bulletinboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AuthenticationApp {
     public static void main(String[] args) {
         SpringApplication.run(AuthenticationApp.class, args);
