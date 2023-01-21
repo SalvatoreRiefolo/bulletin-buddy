fdd77d1f669cf9ed733b5ea47118d0b02b509c87
clarari <cmricken2@gmail.com>
2023-01-21 17:21:34 +0100

    make comment service register with consul

--- bulletin-buddy-comment-service/src/main/java/com/github/bulletinboard/CommentApp.java
+++ bulletin-buddy-comment-service/src/main/java/com/github/bulletinboard/CommentApp.java
@@ -2,9 +2,11 @@ package com.github.bulletinboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CommentApp {
     public static void main(String[] args) {
         SpringApplication.run(CommentApp.class, args);
