db6e54bc64cc35569ef69a4d7832257dc18f82a4
clarari <cmricken2@gmail.com>
2023-01-22 12:51:49 +0100

    remove unused annotation

--- bulletin-buddy-authentication-service/src/main/java/com/github/bulletinboard/AuthenticationApp.java
+++ bulletin-buddy-authentication-service/src/main/java/com/github/bulletinboard/AuthenticationApp.java
@@ -2,10 +2,8 @@ package com.github.bulletinboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class AuthenticationApp {
     public static void main(String[] args) {
         SpringApplication.run(AuthenticationApp.class, args);
