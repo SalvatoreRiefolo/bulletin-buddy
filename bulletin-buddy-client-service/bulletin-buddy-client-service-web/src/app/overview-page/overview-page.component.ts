3ec068553f842c4eab3f3c2d9a9a822a7d2a7c99
Katharina Stengg <katharina.stengg@student.tugraz.at>
2023-01-24 21:21:56 +0100

    small changes in sorting of posts

--- bulletin-buddy-client-service/bulletin-buddy-client-service-web/src/app/overview-page/overview-page.component.ts
+++ bulletin-buddy-client-service/bulletin-buddy-client-service-web/src/app/overview-page/overview-page.component.ts
@@ -43,91 +43,36 @@ export class OverviewPageComponent implements OnInit {
 
   sort(event: string){
     this.sortOption = event;
-    // TODO method for: sort posts according to comment count (the one with the most comments first)
-    /*const getCommentCount = (id: string): number => {
-      let count: number = 0;
-      // TODO take 1 to get first value
-      this.commentsService.getComments(id).pipe(take(1)).subscribe((data: PostComment[]) => {
-        /*data.map( data: PostComment[]) => {
-        }
-        count = data.length;
-        console.log(count);
-        return data.length;
-      });
-
-      return count;
-    }*/
-
     if (this.sortOption == "DESC"){
-      console.log("in desc");
       let comments: PostComment[][] = [];
-      // store pid and amount of comments
-      let myMap = new Map<string, number>();
-      let counter: number = 0;
-
       for (const post of this.filteredPosts) {
-        counter = counter + 1;
-        console.log("Post gefunden");
-        let id: string = post.id;
-        // TODO need subscribe as Observable is returned
-        // TODO do everything inside subscription
-        this.commentsService.getComments(id).subscribe((commentsArray: PostComment[])=>
-          {comments.push(commentsArray)/*) console.log(commentsArray);
-            myMap.set(id, commentsArray.length); console.log(myMap);*/
-
-            // TODO get id's
-
-            // TODO letzter loop run (alles im comments drin)
-            // TODO WORKS + IS SORTED
-            //if(counter == this.filteredPosts.length){
-            console.log(comments.length);
+        this.commentsService.getComments(post.id).subscribe((commentsArray: PostComment[])=>
+          {
+            comments.push(commentsArray)
             if(comments.length == this.filteredPosts.length){
               comments.sort((one,two)=> (one.length > two.length) ? -1:1);
-              console.log(comments);
-
-              /*for (const postComment of comments) {
-                // TODO get pid
-                let pid: string | undefined = postComment.at(0)?.postId;
-
-              }*/
-
               this.filteredPosts.sort((one, two) => {
-                console.log(one.id);
-                console.log(two.id);
-
-                // TODO sort posts according to their amout of comments
-                // TODO works until here
                 let commentOne: PostComment[] | undefined;
                 let commentTwo: PostComment[] | undefined;
-
                 for (const commentIter of comments) {
-                  console.log(commentIter);
                   let commentTemp = commentIter;
                   if(commentTemp.length != 0){
+                    // match posts and comments
                   if(commentTemp[0].postId == one.id){
-                    console.log("True");
                     commentOne = commentIter;
                   }
                   if(commentTemp[0].postId == two.id){
-                    console.log("False");
                     commentTwo = commentIter;
                   }
                   }
                   else{
-                    // TODO Fall wo kein comment vorhanden
-                    if(commentOne == [] || commentOne == undefined){
+                    if(commentOne == undefined){
                       commentOne = commentTemp;
-                    }
-                    if(commentTwo==[] || commentTwo == undefined){
+                    } else if(commentTwo == undefined){
                       commentTwo = commentTemp;
                     }
                   }
                 }
-
-                /*let counterOne: number | undefined = comments?.at(parseInt(one.id))?.length;
-                let counterTwo: number | undefined = comments?.at(parseInt(two.id))?.length;*/
-                console.log(commentOne);
-                console.log(commentTwo);
                 if(commentOne && commentTwo){
                   return (commentOne.length > commentTwo.length ? -1:1);
                 }
@@ -136,16 +81,14 @@ export class OverviewPageComponent implements OnInit {
               }
           });
       }
-
-     /* this.filteredPosts = this.filteredPosts.sort((one, two) =>
-        (getCommentCount(one.id) > getCommentCount(two.id))
-          ? -1 : 1);*/
     }
     else
     {
-      console.log("Default value");
-      // TODO filter as then posts is used and therefore initial sorting is applied
-      this.filter(this.filterOption);
+      this.postsService.getOverviewPosts().subscribe((data: Post[]) => {
+        this.posts = data;
+        this.filteredPosts = data;
+        this.filter(this.filterOption);
+      });
     }
   }
 
