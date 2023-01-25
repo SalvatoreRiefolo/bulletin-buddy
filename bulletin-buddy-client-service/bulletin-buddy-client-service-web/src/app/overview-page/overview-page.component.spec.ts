5c6c8d7b8c9d27fdb96b7a6707eddf1db83943ae
Katharina Stengg <katharina.stengg@student.tugraz.at>
2023-01-25 13:25:10 +0100

    unit tests for checkbox

--- bulletin-buddy-client-service/bulletin-buddy-client-service-web/src/app/overview-page/overview-page.component.spec.ts
+++ bulletin-buddy-client-service/bulletin-buddy-client-service-web/src/app/overview-page/overview-page.component.spec.ts
@@ -1,5 +1,5 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, fakeAsync, TestBed} from '@angular/core/testing';
 import { of } from 'rxjs/internal/observable/of';
 import { Post, PostType } from '../post.model';
 import { PostsService } from '../posts.service';
@@ -51,5 +51,22 @@ describe('OverviewPageComponent', () => {
     expect(component.posts[0]["publisherEmail"]).toEqual('my@email.com');
     expect(component.posts[0]["type"]).toEqual(PostType.OFFER);
   });
+
+  it('should check whether checkbox is unchecked at initial state', fakeAsync(() => {
+    let checkbox = fixture.nativeElement.querySelector('input');
+    fixture.detectChanges();
+    fixture.whenStable().then(()=>{
+      expect(checkbox.checked).toBeFalse();
+    });
+  }));
+
+  it('should check whether click on checkbox works', fakeAsync(() => {
+    let checkbox = fixture.nativeElement.querySelector('input');
+    fixture.detectChanges();
+    checkbox.click();
+    fixture.whenStable().then(()=>{
+      expect(checkbox.checked).toBeTrue();
+    });
+  }));
 
 });
