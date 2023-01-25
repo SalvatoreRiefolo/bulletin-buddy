84cf1a9daa759173038b73549ae2ca16f1c4183a
Pranusha23 <119940119+Pranusha23@users.noreply.github.com>
2023-01-25 14:19:06 +0100

    Deleted "CheckOffers.cy.js" and wrote a test to filter the posts(OFFERS/REQUESTS) in the "LoginChain"

--- bulletin-buddy-client-service/bulletin-buddy-client-service-web/cypress/e2e/FilterOffers.cy.js
+++ bulletin-buddy-client-service/bulletin-buddy-client-service-web/cypress/e2e/FilterOffers.cy.js
@@ -1,9 +1,17 @@
+/////////////////////////// Filter the posts //////////////////////
+// This UI test does the following:
+// - Select filter option after logging in with registered user ID and password
+// - dropdown shows 2 options: 1)Show OFFERS 2)Show REQUESTS
+// - The user logs in with the credentials and checks if the offer/request exists
+///////////////////////////////////////////////////////////////
+
+
 //Select filter option after logging in with registered user ID and password
 //dropdown shows 2 options: 1)Show OFFERS 2)Show REQUESTS
 
 describe("FilterOffersDropdown",()=>{
 
-  it("SelectFilterOption",()=>{
+  it("FilterOffers",()=>{
 
     cy.visit("http://localhost:8080/");
     cy.get("button[class='btn btn-secondary login']").click();
@@ -13,14 +21,24 @@ describe("FilterOffersDropdown",()=>{
     cy.get("#password").type('test');
     cy.get("button[type='submit']").click();
     cy.get("#postType").select('Show OFFERS');
-    cy.get("div[class='container mt-5'] div:nth-child(2) a:nth-child(1) div:nth-child(1) h3:nth-child(1)").click();
+  //  cy.get("div[class='container mt-5'] div:nth-child(2) a:nth-child(1) div:nth-child(1) h3:nth-child(1)").click();
+  cy.get("div:nth-child(3) a:nth-child(1) div:nth-child(1) h3:nth-child(1)").click();
 
 
+})
 
+it("FilterRequests",()=>{
 
+    cy.visit("http://localhost:8080/");
+    cy.get("button[class='btn btn-secondary login']").click();
+    cy.get("#emailAddress").clear();
+    cy.get("#emailAddress").type('test@test.com');
+    cy.get("#password").clear();
+    cy.get("#password").type('test');
+    cy.get("button[type='submit']").click();
+    cy.get("#postType").select('Show REQUESTS');
+    cy.get("div:nth-child(3) a:nth-child(1) div:nth-child(1) h3:nth-child(1)").click();
 
-  //cy.get('.form-outline.mb-4').contains('Show OFFERS').click();
-  //cy.get("label[for='postType']").click();
 
 })
 
--- bulletin-buddy-client-service/bulletin-buddy-client-service-web/cypress/e2e/RegisterChain.cy.js
+++ bulletin-buddy-client-service/bulletin-buddy-client-service-web/cypress/e2e/RegisterChain.cy.js
@@ -1,3 +1,10 @@
+/////////////////////////// Register chain //////////////////////
+// This UI test does the following:
+// 1. Registers a new user
+// 2. The registered new user clicks on 'Add post', then select the PostType(OFFERS/REQUESTS)
+///////////////////////////////////////////////////////////////
+
+
 describe("RegisterChain",()=>{
 
 
@@ -5,7 +12,7 @@ it("Register",()=>{
 // visit the website and register a new user
   cy.visit("http://localhost:8080/");
   cy.get(".btn.btn-secondary.login.mx-1").click();
-  cy.get("#emailAddress").type('010@random.com'); //change the email address everytime to validate the process of registration
+  cy.get("#emailAddress").type('020@random.com'); //change the email address everytime to validate the process of registration
   cy.get("#password").type('sample');
   cy.get("#password_confirm").type('sample');
   cy.get("button[type='submit']").click();
@@ -15,7 +22,7 @@ it("Register",()=>{
    cy.get(".btn.btn-secondary").click();
    cy.get("#title").type("FirstOffer1");
    cy.get("#body").type("OfferBody1");
-    cy.get("#postType").select('OFFER');
+    cy.get("#postType").select('OFFER'); //Change to REQUEST if the user has to post a request
 //   cy.get("#postType").type("OFFER").type("{enter}");
  cy.get("button[type='submit']").click();
 
